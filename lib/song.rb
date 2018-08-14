1df54c3c601fb2c329ed3104b99aa0c6672a11bf
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-14 13:44:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ class Song
   end
 
   def self.find_or_create_by_name(song_name)
-    if self.find_by_name(song_name)==false
+    if self.find_by_name(song_name)== nil
       self.create_by_name(song_name)
     else
       self.find_by_name(song_name)
